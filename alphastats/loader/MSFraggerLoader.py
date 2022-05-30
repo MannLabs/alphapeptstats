6aa13d2213240022dbb6b30b278f42362f467350
elena-krismer <elena.krismer@hotmail.com>
2022-05-30 10:20:52 +0200

    msfragger loader

--- alphastats/loader/MSFraggerLoader.py
+++ alphastats/loader/MSFraggerLoader.py
@@ -1,4 +1,18 @@
 from alphastats.loader import BaseLoader
 
-class MSFraggerLoader(BaseLoader):
-    pass
+# Philosopher
+class FragPipeLoader(BaseLoader):
+    # https://github.com/Nesvilab/MSFragger/wiki/Interpreting-MSFragger-Output
+
+    # SAMPLE
+    # columm Spectrum
+
+    # https://fragpipe.nesvilab.org/docs/tutorial_fragpipe_outputs.html#combined_proteintsv
+    # Frag pipe
+    # https://github.com/Nesvilab/philosopher/wiki/Combined-protein-reports
+    def __init__(self):
+        self.sample_value_column = "[experiment] MaxLFQ Intensity ", # normalized protein intensity using the unique+razor sequences after razor assignment calculated using the MaxLFQ method
+       
+        self.index_column = "Protein",
+        #self.filter_column = "Indistinguishable Proteins"? "Protein Probability"
+        self.software = "MSFragger_Philosopher"
