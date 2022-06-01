fb2809854d374a986ec3755fe805983307bed655
elena-krismer <elena.krismer@hotmail.com>
2022-06-01 21:30:08 +0200

    extract protein ids

--- alphastats/loader/AlphaPeptLoader.py
+++ alphastats/loader/AlphaPeptLoader.py
@@ -29,6 +29,8 @@ class AlphaPeptLoader(BaseLoader):
         self.index_column = index_column
         # add contamination column "Reverse"
         self.add_contamination_column()
+        # make ProteinGroup column 
+        self.rawdata["ProteinGroup"] = self.rawdata[self.index_column].map(self.standardize_protein_group_column)
     
     def load_hdf_protein_table(self, file):
         """_summary_
@@ -47,6 +49,32 @@ class AlphaPeptLoader(BaseLoader):
         False
         )
         self.filter_column = ["Reverse"]
+    
+    def standardize_protein_group_column(self, entry):
+        # make column with ProteinGroup to make comparison between softwares possible
+        #  'sp|P0DMV9|HS71B_HUMAN,sp|P0DMV8|HS71A_HUMAN', -> P0DMV9;P0DMV8
+
+        # This needs a more beautiful and robuster solution
+    
+        # split proteins into list
+        proteins = entry.split(",")
+        protein_id_list = list()
+        for protein in proteins:
+            # 'sp|P0DMV9|HS71B_HUMAN,sp|P0DMV8|HS71A_HUMAN',
+            fasta_header_split = protein.split("|")
+            if len(fasta_header_split) == 1:
+                #  'ENSEMBL:ENSBTAP00000007350',
+                if "ENSEMBL:" in fasta_header_split:
+                    protein_id = fasta_header_split.replace("ENSEMBL:", "")
+                # if only protein id is given
+                else:
+                    protein_id = fasta_header_split[0]
+            else:
+                protein_id = fasta_header_split[1]
+            protein_id_list.append(protein_id)
+        protein_id_concentate = ";".join(protein_id_list)
+        return protein_id_concentate
+
        
        
 
