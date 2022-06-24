b4a51a9f889ab597333b871f1461346b518fee31
elena-krismer <elena.krismer@hotmail.com>
2022-06-24 18:17:35 +0200

    test loaders

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -9,7 +9,7 @@ from alphastats.loader.FragPipeLoader import FragPipeLoader
 
 
 class BaseTestLoader:
-    # parent class of test loader for common tests among loaders
+    #  parent class of test loader for common tests among loaders
     # this is wrapped in a nested class so it doesnt get called separatly when testing
     # plus to avoid multiple inheritance
     class BaseTest(unittest.TestCase):
@@ -20,37 +20,38 @@ class BaseTestLoader:
         def test_check_if_columns_are_present(self):
             # check if columns are present
             # check if error gets raised when column is not present
-            pass
-            
+            self.obj.confidence_column = "wrong_column"
+            self.assertRaises(self.obj.check_if_columns_are_present())
+            self.obj.confidence_column = None
+
         def test_check_if_indexcolumn_is_unique(self):
-            # check if indexcolumn is unique
+            #  check if indexcolumn is unique
             # check if error gets raised when duplicate
-            pass
-    
+            self.obj.rawdata[self.index_column] = "non unique"
+            self.assertRaises(self.obj.if_indexcolumn_is_unique())
+
         def test_check_if_file_exists(self, file):
             # check if error gets raised when file doesnt exist
+            wrong_file_path = "wrong/file/path"
+            self.assertRaises(self.obj.check_if_file_exists(file=wrong_file_path))
 
-            # # check if dataframe is loaded
-            # check dimensions  
-            # self.assertRaises(ValueError)
-            pass
-
-       
 
 class TestAlphaPeptLoader(BaseTestLoader.BaseTest):
     def setUp(self):
         self.obj = AlphaPeptLoader(file="../testfiles/alphapept_results_proteins.csv")
+        # self.hdf_file =""
 
     def test_df_dimensions(self):
         # test if dataframe gets loaded correctly
-        # are there as many rows and column we expect
+        #  are there as many rows and column we expect
         n_rows = self.obj.rawdata.shape[0]
         n_columns = self.obj.rawdata.shape[1]
         self.assertEqual(n_rows, 5)
         self.assertEqual(n_columns, 3781)
 
     def test_load_hdf_protein_table(self):
-        # hdf_format = AlphaPeptLoader(file="../testfiles/alphapept_results.hdf")
+        #  TODO get corresponding HDF file
+        # hdf_format = AlphaPeptLoader(file=self.hdf_file)
         # test if hdf file gets loaded
         # n_rows = hdf_format.shape[0]
         # n_columns = hdf_format.shape[1]
@@ -59,31 +60,48 @@ class TestAlphaPeptLoader(BaseTestLoader.BaseTest):
         pass
 
     def test_add_contamination_column(self):
-         # check if contamination column has been added
-        pass
+        # check if contamination column has been added
+        column_added = "Reverse" in self.obj.rawdata
+        self.assertTrue(column_added)
+        # check if column contains True and False
+        self.assertEqual(self.obj.rawdata.Reverse.dtype, "bool")
 
     def test_standardize_protein_group_column(self):
-        pass
-    
-        # check if contamination column has been added
-        # # self.assertRaises(ValueError)
+        # check if column ProteinGroup has been added
+        column_added = "ProteinGroup" in self.obj.rawdata
+        self.assertTrue(column_added)
+
+        # test function with different entries
+        entry_one = "sp|P0DMV9|HS71B_HUMAN,sp|P0DMV8|HS71A_HUMAN"
+        entry_one_protein_id = self.obj.standardize_protein_group_column(
+            entry=entry_one
+        )
+        self.assertEqual(entry_one_protein_id, "P0DMV9;P0DMV8")
+
+        entry_two = "ENSEMBL:ENSBTAP00000007350"
+        entry_two_protein_id = self.obj.standardize_protein_group_column(
+            entry=entry_two
+        )
+        self.assertEqual(entry_two_protein_id, "ENSBTAP00000007350")
 
 
 class TestMaxQuantLoader(BaseTestLoader.BaseTest):
     def setUp(self):
         self.obj = MaxQuantLoader(file="../testfiles/maxquant_proteinGroups.txt")
-    
+
     def test_df_dimensions(self):
         # test if dataframe gets loaded correctly
-        # are there as many rows and column we expect
+        #  are there as many rows and column we expect
         n_rows = self.obj.rawdata.shape[0]
         n_columns = self.obj.rawdata.shape[1]
         self.assertEqual(n_rows, 2611)
         self.assertEqual(n_columns, 2530)
- 
+
     def test_set_filter_columns_to_true_false(self):
         # check if + has been replaced by TRUE FALSE
-        pass
+        self.assertEqual(self.obj.rawdata["Reverse"].dtype, "bool")
+        self.assertEqual(self.obj.rawdata["Only identified by site"].dtype, "bool")
+        self.assertEqual(self.obj.rawdata["Potential contaminant"].dtype, "bool")
 
 
 class TestDIANNLoader(BaseTestLoader.BaseTest):
@@ -92,7 +110,7 @@ class TestDIANNLoader(BaseTestLoader.BaseTest):
 
     def test_df_dimensions(self):
         # test if dataframe gets loaded correctly
-        # are there as many rows and column we expect
+        #  are there as many rows and column we expect
         n_rows = self.obj.rawdata.shape[0]
         n_columns = self.obj.rawdata.shape[1]
         self.assertEqual(n_rows, 10)
@@ -102,18 +120,15 @@ class TestDIANNLoader(BaseTestLoader.BaseTest):
 class TesFragPipeLoader(BaseTestLoader.BaseTest):
     def setUp(self):
         self.obj = FragPipeLoader(file="../testfiles/fragpipe_combined_proteins.tsv")
-    
+
     def test_df_dimensions(self):
         # test if dataframe gets loaded correctly
-        # are there as many rows and column we expect
+        #  are there as many rows and column we expect
         n_rows = self.obj.rawdata.shape[0]
         n_columns = self.obj.rawdata.shape[1]
         self.assertEqual(n_rows, 10)
         self.assertEqual(n_columns, 36)
 
-       
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
