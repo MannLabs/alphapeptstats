13b8e5419d22bb6eeb81443c9159619176079894
elena-krismer <elena.krismer@hotmail.com>
2022-07-04 13:56:42 +0200

    sample distribution

--- alphastats/_DataSet_Plot.py
+++ alphastats/_DataSet_Plot.py
@@ -4,7 +4,29 @@ import plotly.express as px
 import plotly
 import dash_bio
 
-plotly.io.templates.default = "simple_white"
+# make own alphastats theme
+plotly.io.templates["alphastats_colors"] = plotly.graph_objects.layout.Template(
+    layout=plotly.graph_objects.Layout(
+        colorway=[
+            "#489B80",
+            "#B65EAF",
+            "#7593D6",
+            "#A475D6",
+            "#C177B3",
+            "#468189",
+            "#9DBEBB",
+            "#65676F",
+            "#ABCCA6",
+            "#B395F2",
+            "#535F97",
+            "#A7D87F",
+            "#F3A020",
+            "#20CAF3",
+        ]
+    )
+)
+plotly.io.templates.default = "simple_white+alphastats_colors"
+
 
 class Plot:
     def plot_pca(self, group=None):
@@ -14,11 +36,11 @@ class Plot:
             group (str, optional): column in metadata that should be used for coloring. Defaults to None.
 
         Returns:
-            plotly Object: p
+            plotly.graph_objects._figure.Figure: PCA plot
         """
         if self.imputation is None and self.mat.isna().values.any():
             logging.warning(
-                "Data contains missing values. Missing values will be replaced with 0. Consider Imputation:" 
+                "Data contains missing values. Missing values will be replaced with 0. Consider Imputation:"
                 "for instance `DataSet.preprocess(imputation='mean')`."
             )
 
@@ -61,18 +83,51 @@ class Plot:
             save_figure (bool, optional): _description_. Defaults to False.
 
         Returns:
-            _type_: _description_
+            plotly.graph_objects._figure.Figure: Correlation matrix
         """
         corr_matrix = self.mat.transpose().corr(method=method)
         plot = px.imshow(corr_matrix)
         return plot
 
-    def plot_sampledistribution(self, group=None):
+    def plot_sampledistribution(self, method="violin", color=None, log_scale=True):
+        """Plot Intesity Distribution for each sample. Either Violin or Boxplot
+
+        Args:
+            method (str, optional): Violinplot = "violin", Boxplot = "box". Defaults to "violin".
+            color (_type_, optional): A metadata column used to color the boxes. Defaults to None.
+            log_scale (bool, optional): plot in logarithmic scale. Defaults to True.
+
+        Returns:
+             plotly.graph_objects._figure.Figure: Sample Distribution Plot
+        """
+
+        # create long df
         df = self.mat.unstack().reset_index()
-        fig = px.box(df, x="level_0", y=0)
-        
+        df.rename(columns={"level_1": "sample", 0: "Intensity"}, inplace=True)
+
+        if color is not None:
+            df = df.merge(self.metadata, how="inner", on=["sample"])
+
+        if method == "violin":
+            fig = px.violin(df, x="sample", y="Intensity", color=color)
+        if method == "box":
+            fig = px.box(df, x="sample", y="Intensity", color=color)
+
+        if log_scale:
+            fig.update_layout(yaxis=dict(type="log"))
+        return fig
 
     def plot_volcano(self, column, group1, group2):
+        """Plot Volcano Plot
+
+        Args:
+            column (_type_): _description_
+            group1 (_type_): _description_
+            group2 (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         result = self.calculate_ttest_fc(column, group1, group2)
         result = result.drop_na()
         volcano_plot = dash_bio.VolcanoPlot(
@@ -85,5 +140,26 @@ class Plot:
         )
         return volcano_plot
 
-# Plotly update figures
-# https://maegul.gitbooks.io/resguides-plotly/content/content/plotting_locally_and_offline/python/methods_for_updating_the_figure_or_graph_objects.html
+    def plot_hierarchialclustering(self):
+        # of anova results
+        # general of a subset of proteins
+
+        # use dash bio
+        pass
+
+    def plot_line(self):
+        pass
+
+    def plot_box(self):
+        pass
+
+    def plot_upset(self):
+        pass
+        # Plotly update figures
+        # https://maegul.gitbooks.io/resguides-plotly/content/content/plotting_locally_and_offline/python/methods_for_updating_the_figure_or_graph_objects.html
+
+        """Plot sample distribution
+
+        Returns:
+            plotly.graph_objects._figure.Figure: Plot
+        """
