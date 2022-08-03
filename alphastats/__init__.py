f4173e4f04168632e89e3d1dc10de0161e22f11d
elena-krismer <elena.krismer@hotmail.com>
2022-08-03 14:23:55 +0200

    use init file for config

--- alphastats/__init__.py
+++ alphastats/__init__.py
@@ -1,9 +1,3 @@
-from .loader.AlphaPeptLoader import *
-from .loader.DIANNLoader import *
-from .loader.FragPipeLoader import *
-from .loader.MaxQuantLoader import *
-from .DataSet import *
-
 __project__ = "alphastats"
 __version__ = "0.0.1"
 __license__ = "Apache"
@@ -16,7 +10,7 @@ __keywords__ = [
     "software",
     "mass spectometry",
 ]
-__python_version__ = ">=3.8,<3.10"
+__python_version__ = ">=3.8,<4"
 __classifiers__ = [
     "Development Status :: 1 - Planning",
     # "Development Status :: 2 - Pre-Alpha",
@@ -31,16 +25,22 @@ __classifiers__ = [
     "Programming Language :: Python :: 3",
     "Topic :: Scientific/Engineering :: Bio-Informatics",
 ]
-__console_scripts__ = [
-    "alphastats=alphastats.cli:run",
-]
 __urls__ = {
     "Mann Labs at MPIB": "https://www.biochem.mpg.de/mann",
     "GitHub": __github__,
-    # "ReadTheDocs": None,
-    # "PyPi": None,
+    "ReadTheDocs": "https://mannlabs.github.io/alphastats/",
+    "PyPi": "https://pypi.org/project/alphastats/"
     # "Scientific paper": None,
 }
+__console_scripts__ = [
+    "alphastats=alphastats.cli:run",
+]
 __extra_requirements__ = {
     "development": "requirements_development.txt",
 }
+
+from .loader.AlphaPeptLoader import *
+from .loader.DIANNLoader import *
+from .loader.FragPipeLoader import *
+from .loader.MaxQuantLoader import *
+from .DataSet import *
