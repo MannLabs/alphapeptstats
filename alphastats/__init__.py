44a34bf33f15ef02b0cd092fcb64fa6b380de7c9
elena-krismer <elena.krismer@hotmail.com>
2022-08-23 11:39:45 +0200

    Bump version: 0.0.4 → 0.0.5

--- alphastats/__init__.py
+++ alphastats/__init__.py
@@ -1,5 +1,5 @@
 __project__ = "alphastats"
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __license__ = "Apache"
 __description__ = "An open-source Python package for Mass Spectrometry Analysis"
 __author__ = "Mann Labs"
