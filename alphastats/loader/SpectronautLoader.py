87bb4da1e6c57a718f8e3987fd8c349e8fbe2ea8
elena-krismer <elena.krismer@hotmail.com>
2023-01-04 18:12:02 +0100

    spectronaut implementation #40

--- alphastats/loader/SpectronautLoader.py
+++ alphastats/loader/SpectronautLoader.py
@@ -18,38 +18,58 @@ class SpectronautLoader(BaseLoader):
         gene_names_column="PG.Genes",
         filter_qvalue = True,
         qvalue_cutoff = 0.01,
-        sep="\t",
-        **kwargs
+        sep="\t"
     ):
-
-        """_summary_
+        """Loads Spectronaut output. Will add contamination column for further analysis.
 
         Args:
-            file (_type_): _description_
-            intensity_column (str, optional): 'F.PeakArea'(default) uses not normalized peak area. 'NormalizedPeakArea' uses peak area normalized by Spectronaut.. Defaults to "F.PeakArea".
-            index_column (str, optional): _description_. Defaults to "PG.ProteinGroups".
-            gene_names_column (str, optional): _description_. Defaults to "PG.Genes".
-            sep (str, optional): _description_. Defaults to "\t".
+            file (str): path to Spectronaut outputfile or pandas.DataFrame 
+            intensity_column (str, optional): columns where the intensity of the proteins are given. Defaults to "PG.Quantity".
+            index_column (str, optional): column indicating the protein groups. Defaults to "PG.ProteinGroups".
+            sample_column (str, optional): column that contains sample names used for downstream analysis. Defaults to "R.FileName".
+            gene_names_column (str, optional): column with gene names. Defaults to "PG.Genes".
+            filter_qvalue (bool, optional): will filter out the intensities that have greater than qvalue_cutoff in EG.Qvalue column. Those intensities will be replaced with zero and will be considered as censored missing values for imputation purpose.. Defaults to True.
+            qvalue_cutoff (float, optional): cut off vaéie. Defaults to 0.01.
+            sep (str, optional): file separation of file. Defaults to "\t".
         """
         
         self.software = "Spectronaut"
         self.intensity_column = intensity_column
         self.index_column = index_column
+        self.confidence_column = None
+        self.filter_columns = []
+        self.evidence_df = None 
+        self.gene_names = None
 
         self._read_spectronaut_file(file=file, sep=sep)
 
         if filter_qvalue:
             self._filter_qvalue(qvalue_cutoff=qvalue_cutoff)
 
-        self._reshape_spectronaut(sample_column=sample_column)
+        self._reshape_spectronaut(sample_column=sample_column, gene_names_column=gene_names_column)
+        self._add_contamination_column()
+              
+
+    def _reshape_spectronaut(self, sample_column, gene_names_column):
+        """
+        other proteomics softwares use a wide format (column for each sample)
+        reshape to a wider format
+        """
+        self.rawinput["sample"] = self.rawinput[sample_column] + "_" + self.intensity_column
+        
+        indexing_columns = [self.index_column]
         
         if gene_names_column in self.rawinput.columns.to_list():
             self.gene_names = gene_names_column
-
-    def _reshape_spectronaut(self, sample_column):
-        self.rawdata["sample"] = self.rawdata[sample_column] + "_" + self.intensity_column
-        df = self.rawdata[self.intensity_column, self.index_column, "sample"].drop_duplicates()
-        self.rawdata = df.pivot(columns='sample', index=self.index_column, values=self.intensity_column)
+            indexing_columns += [self.gene_names]
+        
+        keep_columns = [self.intensity_column, "sample"] + indexing_columns
+        
+        df = self.rawinput[keep_columns].drop_duplicates()
+        df = df.pivot(columns='sample', index=indexing_columns, values=self.intensity_column)
+        df.reset_index(inplace=True)
+        
+        self.rawinput = df
         
         self.intensity_column = "[sample]_" + self.intensity_column
 
@@ -57,9 +77,9 @@ class SpectronautLoader(BaseLoader):
         if "EG.Qvalue" not in self.rawinput.columns.to_list():
             raise Warning("Column EG.Qvalue not found in file. File will not be filtered according to q-value.")
         
-        rows_before_filtering = self.rawdata.shape[0]
-        self.rawdata = self.rawdata[self.rawdata["EG.Qvalue"] < qvalue_cutoff]
-        rows_after_filtering = self.rawdata.shape[0]
+        rows_before_filtering = self.rawinput.shape[0]
+        self.rawinput = self.rawinput[self.rawinput["EG.Qvalue"] < qvalue_cutoff]
+        rows_after_filtering = self.rawinput.shape[0]
 
         rows_removed = rows_before_filtering - rows_after_filtering
         logging.info(f"{rows_removed} identification with a qvalue below {qvalue_cutoff} have been removed")
@@ -67,16 +87,16 @@ class SpectronautLoader(BaseLoader):
     
     def _read_spectronaut_file(self, file, sep):
         # some spectronaut files include european decimal separators
-        df = pd.read_csv(file, sep=sep)
+        if isinstance(file, pd.DataFrame):
+            df = file
+        else:
+            df = pd.read_csv(file, sep=sep, low_memory=False)
 
-        if df[self.intensity_column].dtype != np.float64:
-            # load european
-             df = pd.read_csv(file, sep=sep, decimal=",")
+            if df[self.intensity_column].dtype != np.float64:
+                # load european
+                df = pd.read_csv(file, sep=sep, decimal=",")
 
-        if df[self.intensity_column].dtype != np.float64:
-            raise ValueError(f"Error in file format. {self.intensity_column} does not contain float values (numbers).")
-
-        self.rawdata = df
+        self.rawinput = df
         
     
 
@@ -93,3 +113,4 @@ class SpectronautLoader(BaseLoader):
 # Peptide Level
 # F.PeakArea
 # PEP.StrippedSequence
+
