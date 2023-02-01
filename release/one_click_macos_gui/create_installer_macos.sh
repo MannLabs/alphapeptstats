31e227cb70a0eb9c067c9fcdf7509b55b874c6dc
elena-krismer <elena.krismer@hotmail.com>
2023-02-01 12:44:24 +0100

    one click installler

---
+++ release/one_click_macos_gui/create_installer_macos.sh
@@ -0,0 +1,44 @@
+#!bash
+
+# Initial cleanup
+rm -rf dist
+rm -rf build
+FILE=AlphaPeptStats.pkg
+if test -f "$FILE"; then
+  rm AlphaPeptStats.pkg
+fi
+cd ../..
+rm -rf dist
+rm -rf build
+
+# Creating a conda environment
+conda create -n alphapeptstatsinstaller python=3.8 -y
+conda activate alphapeptstatsinstaller
+
+# Creating the wheel
+python setup.py sdist bdist_wheel
+
+# Setting up the local package
+cd release/one_click_macos_gui
+pip install "../../dist/alphastats-0.3.0-py3-none-any.whl"
+
+# Creating the stand-alone pyinstaller folder
+pip install pyinstaller==4.10
+pyinstaller ../pyinstaller/alphastats.spec -y
+conda deactivate
+
+# If needed, include additional source such as e.g.:
+# cp ../../omiclearn/data/*.fasta dist/omiclearn/data
+
+# Wrapping the pyinstaller folder in a .pkg package
+mkdir -p dist/alphastats/Contents/Resources
+cp ../logos/alphapeptstats_logo.icns dist/alphastats/Contents/Resources
+mv dist/alphastats_gui dist/alphastats/Contents/MacOS
+cp Info.plist dist/alphastats/Contents
+cp alphastats_terminal dist/alphastats/Contents/MacOS
+cp ../../LICENSE.txt Resources/LICENSE.txt
+cp ../logos/alphapeptstats_logo.png Resources/alphapeptstats_logo.png
+chmod 777 scripts/*
+
+pkgbuild --root dist/alphastats --identifier de.mpg.biochem.alphastats.app --version 0.3.0 --install-location /Applications/AlphaPeptStats.app --scripts scripts AlphaPeptStats.pkg
+productbuild --distribution distribution.xml --resources Resources --package-path AlphaPeptStats.pkg dist/alphastats_gui_installer_macos.pkg
---
+++ release/pyinstaller/alphastats_pyinstaller.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    import alphastats.gui
+    import multiprocessing
+    multiprocessing.freeze_support()
+    alphastats.gui.run()
