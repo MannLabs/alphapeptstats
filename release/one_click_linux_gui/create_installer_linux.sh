b896699b19aeca18afdea42f8a82bedbc4f9970b
elena-krismer <elena.krismer@hotmail.com>
2023-02-01 17:52:20 +0100

    one click installer fix

--- release/one_click_linux_gui/create_installer_linux.sh
+++ release/one_click_linux_gui/create_installer_linux.sh
@@ -30,7 +30,7 @@ conda deactivate
 
 # Wrapping the pyinstaller folder in a .deb package
 mkdir -p dist/alphastats_gui_installer_linux/usr/local/bin
-mv dist/alphastats dist/alphastats_gui_installer_linux/usr/local/bin/AlphaPeptStats
+mv dist/AlphaPeptStats dist/alphastats_gui_installer_linux/usr/local/bin/AlphaPeptStats
 mkdir dist/alphastats_gui_installer_linux/DEBIAN
 cp control dist/alphastats_gui_installer_linux/DEBIAN
 dpkg-deb --build --root-owner-group dist/alphastats_gui_installer_linux/
