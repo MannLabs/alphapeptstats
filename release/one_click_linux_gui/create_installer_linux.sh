f8776904754274854537b85a2159e70bcbbde9f9
elena-krismer <elena.krismer@hotmail.com>
2023-02-02 17:45:45 +0100

    installer path

--- release/one_click_linux_gui/create_installer_linux.sh
+++ release/one_click_linux_gui/create_installer_linux.sh
@@ -29,8 +29,8 @@ conda deactivate
 # WARNING: this probably does not work!!!!
 
 # Wrapping the pyinstaller folder in a .deb package
-mkdir -p dist/alphastats_gui_installer_linux/usr/local/bin
-mv dist/alphastats /alphastats_gui_installer_linux/usr/local/bin/alphastats
+mkdir -p dist/alphastats_gui_installer_linux/usr/local/bin/alphastats
+mv /home/runner/work/alphapeptstats/alphapeptstats/release/one_click_linux_gui/dist/alphastats /alphastats_gui_installer_linux/usr/local/bin/alphastats
 mkdir dist/alphastats_gui_installer_linux/DEBIAN
 cp control dist/alphastats_gui_installer_linux/DEBIAN
 dpkg-deb --build --root-owner-group dist/alphastats_gui_installer_linux/
