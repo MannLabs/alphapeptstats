1c1b7d52ea44a486e0783a088ac8d73d1d276695
elena-krismer <elena.krismer@hotmail.com>
2023-04-05 19:25:01 +0200

    refactor

--- alphastats/DataSet_Statistics.py
+++ alphastats/DataSet_Statistics.py
@@ -7,36 +7,23 @@ import pingouin
 from alphastats.utils import ignore_warning
 from tqdm import tqdm
 from functools import lru_cache
+from typing import Union
 
 from alphastats.statistics.DifferentialExpressionAnalysis import DifferentialExpressionAnalysis
+from alphastats.statistics.Anova import Anova
 
 
 class Statistics:
-    def _add_metadata_column(self, group1_list, group2_list):
-
-        # create new column in metadata with defined groups
-        metadata = self.metadata
-
-        sample_names = metadata[self.sample].to_list()
-        misc_samples = list(set(group1_list + group2_list) - set(sample_names))
-        if len(misc_samples) > 0:
-            raise ValueError(
-                f"Sample names: {misc_samples} are not described in Metadata."
-            )
-
-        column = "_comparison_column"
-        conditons = [
-            metadata[self.sample].isin(group1_list),
-            metadata[self.sample].isin(group2_list),
-        ]
-        choices = ["group1", "group2"]
-        metadata[column] = np.select(conditons, choices, default=np.nan)
-        self.metadata = metadata
-
-        return column, "group1", "group2"
-
     @ignore_warning(RuntimeWarning)
-    def diff_expression_analysis(self, group1, group2, column=None, method="ttest", perm=10, fdr=0.05):
+    def diff_expression_analysis(
+            self,
+            group1: Union[str, list],
+            group2: Union[str, list],
+            column: str = None,
+            method: str = "ttest",
+            perm: int = 10,
+            fdr: float = 0.05,
+        ) -> pd.DataFrame:
         """Perform differential expression analysis doing a a t-test or Wald test. A wald test will fit a generalized linear model.
 
         Args:
@@ -70,21 +57,8 @@ class Statistics:
         ).perform()
         return df
 
-    def _calculate_foldchange(self, mat_transpose, group1_samples, group2_samples):
-        mat_transpose += 0.00001
-        fc = (
-            mat_transpose[group1_samples].T.mean().values
-            / mat_transpose[group2_samples].T.mean().values
-        )
-        df = pd.DataFrame(
-            {"fc": fc, "log2fc": np.log2(fc)},
-            index=mat_transpose.index,
-            columns=["fc", "log2fc"],
-        )
-        return df
-
     @ignore_warning(RuntimeWarning)
-    def tukey_test(self, protein_id, group, df=None):
+    def tukey_test(self, protein_id:str, group:str, df: pd.DataFrame=None) -> pd.DataFrame:
         """Calculate Pairwise Tukey-HSD post-hoc test
         Wrapper around:
         https://pingouin-stats.org/generated/pingouin.pairwise_tukey.html#pingouin.pairwise_tukey
@@ -129,14 +103,13 @@ class Statistics:
 
         return tukey_df
 
-    #@lru_cache(maxsize=20)
     @ignore_warning(RuntimeWarning)
-    def anova(self, column, protein_ids="all", tukey=True):
+    def anova(self, column:str, protein_ids="all", tukey: bool=True) -> pd.DataFrame:
         """One-way Analysis of Variance (ANOVA)
 
         Args:
             column (str): A metadata column used to calculate ANOVA
-            ids (str or list, optional): ProteinIDs to calculate ANOVA for - dependend variable either ProteinID as string, several ProteinIDs as list or "all" to calculate ANOVA for all ProteinIDs. Defaults to "all".
+            protein_ids (str or list, optional): ProteinIDs to calculate ANOVA for - dependend variable either ProteinID as string, several ProteinIDs as list or "all" to calculate ANOVA for all ProteinIDs. Defaults to "all".
             tukey (bool, optional): Whether to calculate a Tukey-HSD post-hoc test. Defaults to True.
 
         Returns:
@@ -145,76 +118,11 @@ class Statistics:
             * ``'ANOVA_pvalue'``: p-value of ANOVA
             * ``'A vs. B Tukey test'``: Tukey-HSD corrected p-values (each combination represents a column)
         """
-        if protein_ids == "all":
-            protein_ids_list = self.mat.columns.tolist()
-        elif isinstance(protein_ids, str):
-            # convert id to list
-            protein_ids_list = [protein_ids]
-        else:
-            protein_ids_list = protein_ids
-        #  generated list of list with samples
-        subgroup = self.metadata[column].unique().tolist()
-        all_groups = []
-        for sub in subgroup:
-            group_list = self.metadata[self.metadata[column] == sub][
-                self.sample
-            ].tolist()
-            all_groups.append(group_list)
-
-        mat_transpose = self.mat[protein_ids_list].transpose()
-        #  perform rowwise anova
-        p_values = mat_transpose.apply(
-            # generate nested list with values for each group
-            # TODO this can print long list of warnings work around this
-            lambda row: scipy.stats.f_oneway(
-                *[row[elem].values.flatten() for elem in all_groups]
-            )[1],
-            axis=1,
-        )
-        anova_df = pd.DataFrame()
-        anova_df[self.index_column], anova_df["ANOVA_pvalue"] = (
-            p_values.index.tolist(),
-            p_values.values,
-        )
-
-        if tukey:
-            final_df = self._create_tukey_df(
-                anova_df=anova_df, protein_ids_list=protein_ids_list, group=column
-            )
-        else:
-            final_df = anova_df
-        return final_df
-
-    def _create_tukey_df(self, anova_df, protein_ids_list, group):
-        #  combine tukey results with anova results
-        df = (
-            self.mat[protein_ids_list]
-            .reset_index()
-            .rename(columns={"index": self.sample})
-        )
-        df = df.merge(self.metadata, how="inner", on=[self.sample])
-        tukey_df_list = []
-        for protein_id in tqdm(protein_ids_list):
-            tukey_df_list.append(
-                self.tukey_test(df=df, protein_id=protein_id, group=group)
-            )
-        # combine all tukey test results
-        tukey_df = pd.concat(tukey_df_list)
-        # combine anova and tukey test results
-        final_df = anova_df.merge(
-            tukey_df[["comparison", "p-tukey", self.index_column]],
-            how="inner",
-            on=[self.index_column],
-        )
-        final_df = final_df.pivot(
-            index=[self.index_column, "ANOVA_pvalue"],
-            columns=["comparison"],
-            values="p-tukey",
-        )
-        return final_df
+        return Anova(dataset=self, column=column, protein_ids=protein_ids, tukey=tukey).perform()
+        
 
     @lru_cache(maxsize=20)
-    def ancova(self, protein_id, covar, between):
+    def ancova(self, protein_id:str, covar: Union[str, list], between:str) -> pd.DataFrame:
         """Analysis of covariance (ANCOVA) with on or more covariate(s).
         Wrapper around = https://pingouin-stats.org/generated/pingouin.ancova.html
 
--- alphastats/statistics/Anova.py
+++ alphastats/statistics/Anova.py
@@ -4,9 +4,11 @@ import scipy
 import tqdm
 
 class Anova:
-    def __init__(self, dataset, column, protein_ids):
+    def __init__(self, dataset, column, protein_ids, tukey):
+        self.dataset = dataset
         self.column = column
         self.protein_ids = protein_ids
+        self.tukey = tukey
         
     def _get_protein_ids_list(self):
         if self.protein_ids == "all":
@@ -19,52 +21,67 @@ class Anova:
         else:
             self.protein_ids_list = self.protein_ids
 
-    def get_pvalues(self):
+    def perform_anova(self):
         p_values = self.mat_transpose.apply(
-            # generate nested list with values for each group
-            # TODO this can print long list of warnings work around this
             lambda row: scipy.stats.f_oneway(
                 *[row[elem].values.flatten() for elem in self.all_groups]
             )[1],
             axis=1,
         )
+        anova_df = pd.DataFrame()
+        anova_df[self.index_column], anova_df["ANOVA_pvalue"] = (
+            p_values.index.tolist(),
+            p_values.values,
+        )
+        return anova_df
 
     def _prepare_data(self):
         #  generated list of list with samples
         subgroup = self.dataset.metadata[self.column].unique().tolist()
         self.all_groups = []
         for sub in subgroup:
-            group_list = self.metadata[self.dataset.metadata[self.column] == sub][
+            group_list = self.dataset.metadata[self.dataset.metadata[self.column] == sub][
                 self.dataset.sample
             ].tolist()
             self.all_groups.append(group_list)
 
         self.mat_transpose = self.dataset.mat[self.protein_ids_list].transpose()
            
-    def _create_tukey_df(self, anova_df, protein_ids_list, group) -> pd.DataFrame:
+    def _create_tukey_df(self, anova_df) -> pd.DataFrame:
         #  combine tukey results with anova results
         df = (
-            self.mat[protein_ids_list]
+            self.dataset.mat[self.protein_ids_list]
             .reset_index()
-            .rename(columns={"index": self.sample})
+            .rename(columns={"index": self.dataset.sample})
         )
-        df = df.merge(self.metadata, how="inner", on=[self.sample])
+        df = df.merge(self.dataset.metadata, how="inner", on=[self.dataset.sample])
         tukey_df_list = []
-        for protein_id in tqdm(protein_ids_list):
+        for protein_id in tqdm(self.protein_ids_list):
             tukey_df_list.append(
-                self.tukey_test(df=df, protein_id=protein_id, group=group)
+                self.dataset.tukey_test(df=df, protein_id=protein_id, group=self.column)
             )
         # combine all tukey test results
         tukey_df = pd.concat(tukey_df_list)
         # combine anova and tukey test results
         final_df = anova_df.merge(
-            tukey_df[["comparison", "p-tukey", self.index_column]],
+            tukey_df[["comparison", "p-tukey", self.dataset.index_column]],
             how="inner",
-            on=[self.index_column],
+            on=[self.dataset.index_column],
         )
         final_df = final_df.pivot(
-            index=[self.index_column, "ANOVA_pvalue"],
+            index=[self.dataset.index_column, "ANOVA_pvalue"],
             columns=["comparison"],
             values="p-tukey",
         )
         return final_df
+    
+    def perform(self) -> pd.DataFrame:
+        self._get_protein_ids_list()
+        self._prepare_data()
+        anova_df = self.perform_anova()
+        
+        if self.tukey:
+            anova_df = self._create_tukey_df(anova_df=anova_df)
+        
+        return anova_df
+
