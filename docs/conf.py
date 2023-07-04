348fc7b4328a1117d6092f3fdd1e86b6e03f0801
elena-krismer <elena.krismer@hotmail.com>
2023-07-04 10:41:22 +0200

    bumpversion 0.6.2 -> 0.6.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Elena Krismer"
 author = "Elena Krismer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.6.2"
+release = "0.6.3"
 
 
 # -- General configuration ---------------------------------------------------
