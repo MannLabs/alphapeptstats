ddc130918a8bf04ec7522f3ca6dab7e2e93a903b
Mikhail Lebedev <lebedev_mikhail@outlook.com>
2024-05-24 13:55:55 +0200

    Bump version: 0.6.5 → 0.6.6

--- alphastats/__init__.py
+++ alphastats/__init__.py
@@ -1,5 +1,5 @@
 __project__ = "alphastats"
-__version__ = "0.6.5"
+__version__ = "0.6.6"
 __license__ = "Apache"
 __description__ = "An open-source Python package for Mass Spectrometry Analysis"
 __author__ = "Mann Labs"
