de75875ef3dae964de63256a3d21a549c0f7c8b3
Julia Schessner <schessner@biochem.mpg.de>
2024-08-29 15:35:27 +0200

    Test for loading maxquant testing data without change of column selection.

--- alphastats/gui/test/test_02_Import Data.py
+++ alphastats/gui/test/test_02_Import Data.py
@@ -4,11 +4,16 @@ from unittest.mock import MagicMock, patch
 import pandas as pd
 from io import BytesIO
 
+def print_session_state(apptest: AppTest):
+    for k,v in apptest.session_state.filtered_state.items():
+        print(f"{k}:    {str(type(v))}   {str(v)[:20] if type(v) not in [int, list, str] else v}")
+
 APP_FOLDER = Path(__file__).parent / Path("../")
 APP_FILE = f"{APP_FOLDER}/pages/02_Import Data.py"
+TEST_FILES = f"{APP_FOLDER}/../../testfiles"
 
 def test_loadpage():
-    at = AppTest(APP_FILE, default_timeout=20)
+    at = AppTest(APP_FILE, default_timeout=200)
     at.run()
 
     assert at.session_state.organism == 9606
@@ -19,9 +24,7 @@ def test_loadpage():
 
 @patch("streamlit.file_uploader")
 def test_loadmockedpage(mock_file: MagicMock):
-    mock_file.return_value = None
-
-    at = AppTest(APP_FILE, default_timeout=100)
+    at = AppTest(APP_FILE, default_timeout=200)
     at.run()
 
     assert at.session_state.organism == 9606
@@ -31,7 +34,7 @@ def test_loadmockedpage(mock_file: MagicMock):
     assert at.session_state.loader == None
 
 def test_sampledata():
-    at = AppTest(APP_FILE, default_timeout=20)
+    at = AppTest(APP_FILE, default_timeout=200)
     at.run()
 
     at.button[0].click().run()
@@ -43,4 +46,55 @@ def test_sampledata():
     assert str(type(at.session_state.loader)) == "<class 'alphastats.loader.MaxQuantLoader.MaxQuantLoader'>"
     assert "plotting_options" in at.session_state
     assert "statistic_options" in at.session_state
+
+@patch("streamlit.file_uploader")
+def test_mqupload(mock_file: MagicMock):
+    def data_buf():
+        with open(f"{TEST_FILES}/maxquant/proteinGroups.txt", "rb") as f:
+            buf = BytesIO(f.read())
+            buf.name = "proteinGroups.txt"
+            return buf
+    def metadata_buf():
+        with open(f"{TEST_FILES}/maxquant/metadata.xlsx", "rb") as f:
+            buf = BytesIO(f.read())
+            buf.name = "metadata.xlsx"
+            at.session_state.metadatafile = buf
+            return buf
+
+    at = AppTest(APP_FILE, default_timeout=200)
+    at.run()
+    
+    at.selectbox(key='software').select('MaxQuant')
+    mock_file.side_effect = [None]
+    at.run()
+
+    mock_file.side_effect = [data_buf(),None]
+    at.run()
+
+    mock_file.side_effect = [data_buf(),metadata_buf()]
+    at.run()
+    assert str(type(at.session_state.loader)) == "<class 'alphastats.loader.MaxQuantLoader.MaxQuantLoader'>"
+    assert at.session_state.intensity_column == 'LFQ intensity [sample]'
+    assert str(type(at.session_state.metadatafile)) == "<class '_io.BytesIO'>"
+    assert at.session_state.software == 'MaxQuant'
+    assert at.session_state.index_column == 'Protein IDs'
+    assert at.session_state.metadata_columns == ['sample']
+    assert at.session_state.sample_column == 'sample'
+
+    mock_file.side_effect = [data_buf(),metadata_buf()]
+    at.button[0].click()
+    at.run()
+    assert at.session_state.dataset.gene_names == "Gene names"
+    assert at.session_state.dataset.index_column == "Protein IDs"
+    assert at.session_state.dataset.intensity_column == 'LFQ intensity [sample]'
+    assert at.session_state.dataset.rawmat.shape == (312, 2611)
+    assert at.session_state.dataset.software == 'MaxQuant'
+    assert at.session_state.dataset.sample == 'sample'
+    assert str(type(at.session_state.distribution_plot)) == "<class 'plotly.graph_objs._figure.Figure'>"
+    assert str(type(at.session_state.loader)) == "<class 'alphastats.loader.MaxQuantLoader.MaxQuantLoader'>"
+    assert "plotting_options" in at.session_state
+    assert "statistic_options" in at.session_state
+
+if __name__ == "__main__":
+    test_mqupload()
 
