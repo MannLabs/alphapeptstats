f588fa01c8f83a4ed2d7a1b42a8a7fa2059c75f8
Julia Schessner <schessner@biochem.mpg.de>
2024-08-29 13:20:48 +0200

    Add test for mocked input page

--- alphastats/gui/test/test_02_Import Data.py
+++ alphastats/gui/test/test_02_Import Data.py
@@ -1,7 +1,27 @@
 from streamlit.testing.v1 import AppTest
+from pathlib import Path
+from unittest.mock import MagicMock, patch
+import pandas as pd
+from io import BytesIO
+
+APP_FOLDER = Path(__file__).parent / Path("../")
+APP_FILE = f"{APP_FOLDER}/pages/02_Import Data.py"
 
 def test_loadpage():
-    at = AppTest("alphastats/gui/pages/02_Import Data.py", default_timeout=20)
+    at = AppTest(APP_FILE, default_timeout=20)
+    at.run()
+
+    assert at.session_state.organism == 9606
+    assert at.session_state.user_session_id == 'test session id'
+    assert at.session_state.software == '<select>'
+    assert at.session_state.gene_to_prot_id == {}
+    assert at.session_state.loader == None
+
+@patch("streamlit.file_uploader")
+def test_loadmockedpage(mock_file: MagicMock):
+    mock_file.return_value = None
+
+    at = AppTest(APP_FILE, default_timeout=100)
     at.run()
 
     assert at.session_state.organism == 9606
@@ -11,7 +31,7 @@ def test_loadpage():
     assert at.session_state.loader == None
 
 def test_sampledata():
-    at = AppTest("alphastats/gui/pages/02_Import Data.py", default_timeout=20)
+    at = AppTest(APP_FILE, default_timeout=20)
     at.run()
 
     at.button[0].click().run()
@@ -23,3 +43,4 @@ def test_sampledata():
     assert str(type(at.session_state.loader)) == "<class 'alphastats.loader.MaxQuantLoader.MaxQuantLoader'>"
     assert "plotting_options" in at.session_state
     assert "statistic_options" in at.session_state
+
