4bf6492c7bd5d1d88298f52992df5c22464c7ecb
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-09-09 12:41:20 +0200

    use new method to plot raw intensity distribution

--- alphastats/gui/pages/03_Data Overview.py
+++ alphastats/gui/pages/03_Data Overview.py
@@ -24,40 +24,41 @@ except ModuleNotFoundError:
 
 sidebar_info()
 
-if "dataset" in st.session_state:
-    st.markdown("### DataSet Info")
+if "dataset" not in st.session_state:
+    st.info("Import Data first")
+    st.stop()
 
-    display_loaded_dataset(st.session_state["dataset"])
+st.markdown("### DataSet Info")
 
-    st.markdown("## DataSet overview")
+display_loaded_dataset(st.session_state["dataset"])
 
-    c1, c2 = st.columns(2)
+st.markdown("## DataSet overview")
 
-    with c1:
-        st.markdown("**Intensity distribution raw data per sample**")
-        st.plotly_chart(
-            st.session_state["distribution_plot"].update_layout(plot_bgcolor="white"),
-            use_container_width=True,
-        )
+c1, c2 = st.columns(2)
 
-    with c2:
-        st.markdown("**Intensity distribution data per sample used for analysis**")
-        st.plotly_chart(
-            get_intensity_distribution_processed(
-                st.session_state.user_session_id
-            ).update_layout(plot_bgcolor="white"),
-            use_container_width=True,
-        )
-
+with c1:
+    st.markdown("**Intensity distribution raw data per sample**")
     st.plotly_chart(
-        get_sample_histogram_matrix(st.session_state.user_session_id).update_layout(
-            plot_bgcolor="white"
-        ),
+        get_intensity_distribution_unprocessed(
+            st.session_state.user_session_id
+        ).update_layout(plot_bgcolor="white"),
         use_container_width=True,
     )
 
-    display_matrix()
+with c2:
+    st.markdown("**Intensity distribution data per sample used for analysis**")
+    st.plotly_chart(
+        get_intensity_distribution_processed(
+            st.session_state.user_session_id
+        ).update_layout(plot_bgcolor="white"),
+        use_container_width=True,
+    )
 
+st.plotly_chart(
+    get_sample_histogram_matrix(st.session_state.user_session_id).update_layout(
+        plot_bgcolor="white"
+    ),
+    use_container_width=True,
+)
 
-else:
-    st.info("Import Data first")
+display_matrix()
