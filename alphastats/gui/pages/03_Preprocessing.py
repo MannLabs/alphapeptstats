84b215ccb663deef3eef9423ccb791eff1224652
Julia Schessner <schessner@biochem.mpg.de>
2024-09-09 16:37:25 +0200

    ruff format

--- alphastats/gui/pages/03_Preprocessing.py
+++ alphastats/gui/pages/03_Preprocessing.py
@@ -2,46 +2,83 @@ import streamlit as st
 import pandas as pd
 
 
-from alphastats.gui.utils.preprocessing_helper import draw_predefined_workflow, configure_preprocessing, update_workflow, run_preprocessing, reset_preprocessing
+from alphastats.gui.utils.preprocessing_helper import (
+    draw_predefined_workflow,
+    configure_preprocessing,
+    update_workflow,
+    run_preprocessing,
+    reset_preprocessing,
+)
 from alphastats.gui.utils.ui_helper import sidebar_info
 
 
 sidebar_info()
 
-if 'workflow' not in st.session_state:
-    st.session_state.workflow = ["remove contaminations", "subset data", "log2 transform"]
+if "workflow" not in st.session_state:
+    st.session_state.workflow = [
+        "remove contaminations",
+        "subset data",
+        "log2 transform",
+    ]
 
 st.markdown("### Preprocessing")
-st.markdown('Select either the predefined workflow where you can only enable/disable steps or create a custom workflow, that allows switching steps around.')
+st.markdown(
+    "Select either the predefined workflow where you can only enable/disable steps or create a custom workflow, that allows switching steps around."
+)
 
 
-tab1, tab2  = st.tabs(["Predefined workflow", "Custom workflow"])
+tab1, tab2 = st.tabs(["Predefined workflow", "Custom workflow"])
 
 with tab1:
-
     c1, c2 = st.columns([1, 1])
 
     with c2:
-    
-        remove_contaminations, remove_samples, subset, data_completeness, log2_transform, normalization, imputation, batch = configure_preprocessing()
+        (
+            remove_contaminations,
+            remove_samples,
+            subset,
+            data_completeness,
+            log2_transform,
+            normalization,
+            imputation,
+            batch,
+        ) = configure_preprocessing()
 
-        update_workflow(remove_contaminations, remove_samples, subset, data_completeness, log2_transform, normalization, imputation, batch)
+        update_workflow(
+            remove_contaminations,
+            remove_samples,
+            subset,
+            data_completeness,
+            log2_transform,
+            normalization,
+            imputation,
+            batch,
+        )
 
     with c1:
         st.write("### Flowchart of currenlty selected workflow:")
 
         selected_nodes = draw_predefined_workflow(st.session_state.workflow)
 
-        if 'dataset' in st.session_state:
+        if "dataset" in st.session_state:
             c11, c12 = st.columns([1, 1])
 
             with c11:
                 if st.button("Run preprocessing"):
-                    run_preprocessing(remove_contaminations, remove_samples, subset, data_completeness, log2_transform, normalization, imputation, batch)
+                    run_preprocessing(
+                        remove_contaminations,
+                        remove_samples,
+                        subset,
+                        data_completeness,
+                        log2_transform,
+                        normalization,
+                        imputation,
+                        batch,
+                    )
 
             with c12:
                 reset_preprocessing()
-        
+
         else:
             st.info("Import Data first")
 
--- alphastats/gui/utils/preprocessing_helper.py
+++ alphastats/gui/utils/preprocessing_helper.py
@@ -4,75 +4,97 @@ from st_cytoscape import cytoscape
 
 import datetime
 
-def draw_predefined_workflow(workflow = ["remove contaminations", "subset data", "log2 transform"]):
-    available_steps = ["remove contaminations", "remove samples", "subset data", "filter data completeness", "log2 transform", "normalization", "imputation", "batch correction"]
+
+def draw_predefined_workflow(
+    workflow=["remove contaminations", "subset data", "log2 transform"],
+):
+    available_steps = [
+        "remove contaminations",
+        "remove samples",
+        "subset data",
+        "filter data completeness",
+        "log2 transform",
+        "normalization",
+        "imputation",
+        "batch correction",
+    ]
 
     elements = [
         {
-            'group': 'nodes',
-            'data': {
-                'id': i,
-                'label': label,
+            "group": "nodes",
+            "data": {
+                "id": i,
+                "label": label,
             },
             "selectable": True,
-            "classes": ['active'] if label in st.session_state.workflow else ['inactive']
-        } for i, label in enumerate(available_steps)
+            "classes": ["active"]
+            if label in st.session_state.workflow
+            else ["inactive"],
+        }
+        for i, label in enumerate(available_steps)
     ]
 
-    for label1, label2 in zip(st.session_state.workflow[:-1], st.session_state.workflow[1:]):
+    for label1, label2 in zip(
+        st.session_state.workflow[:-1], st.session_state.workflow[1:]
+    ):
         i = available_steps.index(label1)
         j = available_steps.index(label2)
-        elements.append({'group':'edges', 'data': {'id': f'{i}_{j}', 'source': i, 'target': j}, 'selectable': False})
+        elements.append(
+            {
+                "group": "edges",
+                "data": {"id": f"{i}_{j}", "source": i, "target": j},
+                "selectable": False,
+            }
+        )
 
     stylesheet = [
-    {
-        "selector": "node",
-        "style": {
-            "label": "data(label)",
-            'shape':'roundrectangle',
-            "width": 200,
-            "height": 60,
-            "text-valign": "center",
-            "text-halign": "center"
+        {
+            "selector": "node",
+            "style": {
+                "label": "data(label)",
+                "shape": "roundrectangle",
+                "width": 200,
+                "height": 60,
+                "text-valign": "center",
+                "text-halign": "center",
+            },
         },
-    },
-    {
-        "selector": "node.active",
-        "style": {
-            "background-color": 'lightgreen',
+        {
+            "selector": "node.active",
+            "style": {
+                "background-color": "lightgreen",
+            },
         },
-    },
-    {
-        "selector": "node.inactive",
-        "style": {
-            "background-color": 'lightgrey',
+        {
+            "selector": "node.inactive",
+            "style": {
+                "background-color": "lightgrey",
+            },
         },
-    },
-    {
-        "selector": "node.selected",
-        "style": {
-            "background-color": 'red'
+        {
+            "selector": "node.selected",
+            "style": {"background-color": "red"},
         },
-    },
-    {
-        "selector": "edge",
-        "style": {
-            "width": 3,
-            "curve-style": "bezier",
-            "target-arrow-shape": "triangle",
+        {
+            "selector": "edge",
+            "style": {
+                "width": 3,
+                "curve-style": "bezier",
+                "target-arrow-shape": "triangle",
+            },
         },
-    },
     ]
 
     selected = cytoscape(
         elements,
         stylesheet,
-        layout={'name': 'grid', 'columns': 1},
-        selection_type='single',
+        layout={"name": "grid", "columns": 1},
+        selection_type="single",
         user_panning_enabled=False,
         user_zooming_enabled=False,
-        height=f'{len(available_steps)*80}px',
-        key="predefined_workflow")
+        height=f"{len(available_steps)*80}px",
+        key="predefined_workflow",
+    )
 
     return selected
 
@@ -96,7 +118,9 @@ def configure_preprocessing():
 
     remove_samples = st.multiselect(
         "Remove samples from analysis",
-        options=[] if 'dataset' not in st.session_state else st.session_state.dataset.metadata[
+        options=[]
+        if "dataset" not in st.session_state
+        else st.session_state.dataset.metadata[
             st.session_state.dataset.sample
         ].to_list(),
     )
@@ -123,18 +147,75 @@ def configure_preprocessing():
     )
 
     batch = st.selectbox(
-            "Batch", options=[False] if 'dataset' not in st.session_state else [False] + st.session_state.dataset.metadata.columns.to_list()
-        )
+        "Batch",
+        options=[False]
+        if "dataset" not in st.session_state
+        else [False] + st.session_state.dataset.metadata.columns.to_list(),
+    )
 
-    return remove_contaminations, remove_samples, subset, data_completeness, log2_transform, normalization, imputation, batch
+    return (
+        remove_contaminations,
+        remove_samples,
+        subset,
+        data_completeness,
+        log2_transform,
+        normalization,
+        imputation,
+        batch,
+    )
 
-def update_workflow(remove_contaminations, remove_samples, subset, data_completeness, log2_transform, normalization, imputation, batch):
+
+def update_workflow(
+    remove_contaminations,
+    remove_samples,
+    subset,
+    data_completeness,
+    log2_transform,
+    normalization,
+    imputation,
+    batch,
+):
     old_workflow = st.session_state.workflow
-    st.session_state.workflow = [el for el, form in zip(["remove contaminations", "remove samples", "subset data", "filter data completeness", "log2 transform", "normalization", "imputation", "batch correction"], [remove_contaminations, remove_samples, subset, data_completeness, log2_transform, normalization, imputation, batch]) if form not in [None, False, [], 0.0]]
+    st.session_state.workflow = [
+        el
+        for el, form in zip(
+            [
+                "remove contaminations",
+                "remove samples",
+                "subset data",
+                "filter data completeness",
+                "log2 transform",
+                "normalization",
+                "imputation",
+                "batch correction",
+            ],
+            [
+                remove_contaminations,
+                remove_samples,
+                subset,
+                data_completeness,
+                log2_transform,
+                normalization,
+                imputation,
+                batch,
+            ],
+        )
+        if form not in [None, False, [], 0.0]
+    ]
     if old_workflow != st.session_state.workflow:
         st.rerun()
 
-def run_preprocessing(remove_contaminations, remove_samples, subset, data_completeness, log2_transform, normalization, imputation, batch):
+
+def run_preprocessing(
+    remove_contaminations,
+    remove_samples,
+    subset,
+    data_completeness,
+    log2_transform,
+    normalization,
+    imputation,
+    batch,
+):
     st.session_state.dataset.preprocess(
         remove_contaminations=remove_contaminations,
         log2_transform=log2_transform,
@@ -162,7 +243,8 @@ def reset_preprocessing():
         st.session_state.dataset.create_matrix()
         preprocessing = st.session_state.dataset.preprocessing_info
         st.info(
-            "Data has been reset. " + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            "Data has been reset. "
+            + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         )
         st.dataframe(
             pd.DataFrame.from_dict(preprocessing, orient="index").astype(str),
