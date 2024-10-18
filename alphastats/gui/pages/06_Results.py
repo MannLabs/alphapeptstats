b564d5c058068970dab01ad3dd649a242e1c73bd
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-10-18 14:49:16 +0200

    simplify results page

--- alphastats/gui/pages/06_Results.py
+++ alphastats/gui/pages/06_Results.py
@@ -1,33 +1,11 @@
-import pandas as pd
 import streamlit as st
 
 from alphastats.gui.utils.analysis_helper import display_plot
 from alphastats.gui.utils.ui_helper import (
     StateKeys,
-    convert_df,
     init_session_state,
     sidebar_info,
 )
-
-
-def display_plotly_figure(plot):
-    st.plotly_chart(plot)
-
-
-def download_preprocessing_info(plot, name, count):
-    preprocesing_dict = plot.preprocessing
-    df = pd.DataFrame(preprocesing_dict.items())
-    filename = "plot" + name + "preprocessing_info.csv"
-    csv = convert_df(df)
-    print("preprocessing" + count)
-    st.download_button(
-        "Download DataSet Info as .csv",
-        csv,
-        filename,
-        "text/csv",
-        key="preprocessing" + count,
-    )
-
 
 init_session_state()
 sidebar_info()
@@ -44,10 +22,7 @@ for count, saved_item in enumerate(st.session_state[StateKeys.PLOT_LIST]):
     method = saved_item[0]
     plot = saved_item[1]
 
-    if method == "ttest":
-        plot = plot.plot
-
-    st.markdown("\n\n")
+    st.markdown("\n\n\n")
     st.markdown(f"#### {method}")
 
     display_plot(method + str(count), plot, show_save_button=False)
