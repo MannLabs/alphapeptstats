2202a24208005b7e0942de895107770363987f21
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2024-10-31 17:07:59 +0100

    polish preprocessing page

--- alphastats/gui/pages/03_Preprocessing.py
+++ alphastats/gui/pages/03_Preprocessing.py
@@ -15,34 +15,34 @@ sidebar_info()
 
 
 st.markdown("### Preprocessing")
-c1, c2 = st.columns([1, 1])
+
+if StateKeys.DATASET not in st.session_state:
+    st.info("Import data first.")
+    st.stop()
+
+c1, _, c2 = st.columns([0.3, 0.1, 0.45])
+
+dataset = st.session_state[StateKeys.DATASET]
+# TODO add help to steps
+with c1:
+    st.write("##### Select preprocessing steps")
+    settings = configure_preprocessing(dataset=st.session_state[StateKeys.DATASET])
+    new_workflow = update_workflow(settings)
+    if new_workflow != st.session_state[StateKeys.WORKFLOW]:
+        st.session_state[StateKeys.WORKFLOW] = new_workflow
+
+    c11, c12 = st.columns([1, 1])
+    if c11.button("Run preprocessing", key="_run_preprocessing"):
+        run_preprocessing(settings, dataset)
+        # TODO show more info about the preprocessing steps
+
+    if c12.button("❌ Reset preprocessing", key="_reset_preprocessing"):
+        reset_preprocessing(dataset)
 
 with c2:
-    if StateKeys.DATASET in st.session_state:
-        settings = configure_preprocessing(dataset=st.session_state[StateKeys.DATASET])
-        new_workflow = update_workflow(settings)
-        if new_workflow != st.session_state[StateKeys.WORKFLOW]:
-            st.session_state[StateKeys.WORKFLOW] = new_workflow
-
-with c1:
-    st.write("#### Flowchart of preprocessing workflow:")
-
     selected_nodes = draw_workflow(st.session_state[StateKeys.WORKFLOW])
 
-    if StateKeys.DATASET not in st.session_state:
-        st.info("Import data first to configure and run preprocessing")
-
-    else:
-        dataset = st.session_state[StateKeys.DATASET]
-
-        c11, c12 = st.columns([1, 1])
-        if c11.button("Run preprocessing", key="_run_preprocessing"):
-            run_preprocessing(settings, dataset)
-            # TODO show more info about the preprocessing steps
-            display_preprocessing_info(dataset.preprocessing_info)
-
-        if c12.button("❌ Reset all Preprocessing steps", key="_reset_preprocessing"):
-            reset_preprocessing(dataset)
-            display_preprocessing_info(dataset.preprocessing_info)
+    st.markdown("##### Current preprocessing status")
+    display_preprocessing_info(dataset.preprocessing_info)
 
 # TODO: Add comparison plot of intensity distribution before and after preprocessing
