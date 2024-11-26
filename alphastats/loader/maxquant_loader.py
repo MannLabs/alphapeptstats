0df1df347631ee5c47e3ff5d5552ab705e30a484
Julia Schessner <schessner@biochem.mpg.de>
2024-11-26 14:06:44 +0100

    Document maxquant data handling better

--- alphastats/loader/maxquant_loader.py
+++ alphastats/loader/maxquant_loader.py
@@ -24,6 +24,8 @@ class MaxQuantLoader(BaseLoader):
     ):
         """Loader MaxQuant output
 
+        Special handling for Maxquant data includes removal of additional input rows that stems from overflowing id references. This is done by checking if the Protein IDs contain at least one letter, as the overflow is a string of numebrs a semicolons. If numeric ids are used, filter the data for any non-nan values in the intensity columns.
+
         Args:
             file (str): ProteinGroups.txt file: http://www.coxdocs.org/doku.php?id=maxquant:table:proteingrouptable
             intensity_column (str, optional): columns with Intensity values for each sample. Defaults to "LFQ intentsity [experiment]".
@@ -46,19 +48,12 @@ class MaxQuantLoader(BaseLoader):
         self._set_filter_columns_to_true_false()
         self._read_all_column_names_as_string()
 
-        # TODO externalize to a method
-        intensity_columns = [
-            col
-            for col in self.rawinput.columns
-            if intensity_column.replace("[sample]", "") in col
-        ]
-        # TODO: explain why we do this
+        intensity_columns = self._get_intensity_columns()
         if len(self.rawinput.dropna(subset=intensity_columns, how="all")) != len(
             self.rawinput
         ):
-            valid_id = re.compile(
-                "[A-Z]"
-            )  # Assuming that all valid protein ids would contain at least one letter.
+            # there are likely overflowing id rows
+            valid_id = re.compile("[A-Z]")
             self.rawinput = self.rawinput[
                 self.rawinput[self.index_column].apply(
                     lambda x: isinstance(x, str) and bool(valid_id.match(x))
