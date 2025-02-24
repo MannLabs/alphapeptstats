8c41b213b5463c07a424a528281e33322d6a4130
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-02-24 16:12:27 +0100

    don't use internal name in metadata template creation

--- alphastats/gui/utils/import_helper.py
+++ alphastats/gui/utils/import_helper.py
@@ -11,6 +11,7 @@ from alphastats.dataset.dataset import DataSet
 from alphastats.gui.utils.options import SOFTWARE_OPTIONS
 from alphastats.loader.base_loader import BaseLoader
 from alphastats.loader.maxquant_loader import MaxQuantLoader
+from dataset.keys import Cols
 
 
 def load_proteomics_data(uploaded_file, intensity_column, index_column, software):
@@ -250,8 +251,9 @@ def show_button_download_metadata_template_file(loader: BaseLoader) -> None:
     buffer = io.BytesIO()
 
     with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
-        # Write each dataframe to a different worksheet.
-        dataset.metadata.to_excel(writer, sheet_name="Sheet1", index=False)
+        # map the internal column name to something else to avoid collisions
+        dataset.metadata.rename(columns={Cols.SAMPLE: "sample"}).to_excel(writer, sheet_name="Sheet1", index=False)
+
 
     st.download_button(
         label="Download Excel template for metadata",
