f49c9b40a041a796419d1dd6aaf6ece97054ff07
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-03-11 09:43:18 +0100

    load sessions

--- alphastats/gui/AlphaPeptStats.py
+++ alphastats/gui/AlphaPeptStats.py
@@ -5,6 +5,7 @@ import streamlit as st
 from alphastats.gui.utils.state_utils import (
     init_session_state,
 )
+from alphastats.gui.utils.session_manager import STATE_SAVE_FOLDER, SessionManager
 from alphastats.gui.utils.ui_helper import (
     img_to_bytes,
     sidebar_info,
@@ -13,6 +14,7 @@ from alphastats.gui.utils.ui_helper import (
 st.set_page_config(layout="wide")
 
 init_session_state()
+sidebar_info()
 
 img_center = """
 <head>
@@ -22,7 +24,7 @@ img {
   display: block;
   margin-left: auto;
   margin-right: auto;
-  width: 33%;
+  width: 25%;
 </style>
 </head>
 """
@@ -37,14 +39,27 @@ st.markdown(
     header_html,
     unsafe_allow_html=True,
 )
-
-##
 st.markdown(
     """\n\n
 An open-source Python package for the analysis of mass spectrometry based proteomics data
 from the [Mann Group at the University of Copenhagen](https://www.cpr.ku.dk/research/proteomics/mann/).
+"""
+)
 
-### How to contribute
+
+st.markdown("""### Load previous session""")
+file_to_load = st.selectbox(
+    options=SessionManager.get_saved_sessions(STATE_SAVE_FOLDER),
+    label="Select a session to load",
+)
+
+if st.button("Load"):
+    SessionManager().load(file_to_load)
+
+
+##
+st.markdown(
+    """### How to contribute
 
 If you like this software, you can give us a [star](https://github.com/MannLabs/alphapeptstats/stargazers) to boost
 our visibility! All direct contributions are also welcome. Feel free to post a new [issue](https://github.com/MannLabs/alphapeptstats/issues)
@@ -53,8 +68,6 @@ interactive participation, check out the [discussions](https://github.com/MannLa
 [the Contributors License Agreement](misc/CLA.md).
 """
 )
-
-sidebar_info()
 
 # TODO The emojis on the sidebar menu look ugly but currently there are no other options to add icon in streamlit multipage apps
 # https://discuss.streamlit.io/t/icons-for-the-multi-app-page-menu-in-the-sidebar-other-than-emojis/27222
