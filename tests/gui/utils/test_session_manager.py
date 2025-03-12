a84cbb6b9f131cce686649fab66e4d4d2bd1bf6c
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-03-12 17:00:25 +0100

    improve tests

--- tests/gui/utils/test_session_manager.py
+++ tests/gui/utils/test_session_manager.py
@@ -1,6 +1,7 @@
 """Tests for the SessionManager class."""
 
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 from streamlit.runtime.state import SessionStateProxy
@@ -8,7 +9,10 @@ from streamlit.runtime.state import SessionStateProxy
 from alphastats.gui.utils.session_manager import SessionManager
 from alphastats.gui.utils.state_keys import StateKeys
 
-EXPECTED_STATE = {StateKeys.USER_SESSION_ID: "some_id"}
+EXPECTED_STATE = {
+    StateKeys.USER_SESSION_ID: "some_id",
+    StateKeys.DATASET: "some_dataset",
+}
 
 
 @pytest.fixture
@@ -27,13 +31,17 @@ def mock_session_state():
 
 def test_copy(mock_session_state):
     """Test that the session state is copied correctly."""
-    target = {}
+
+    already_present_dict = {"already_present_key": "already_present_value"}  # preserved
+    target = already_present_dict | {
+        StateKeys.DATASET: "some_other_dataset"
+    }  # overwritten
 
     session_manager = SessionManager()
 
     # when
     session_manager._copy(mock_session_state.to_dict(), target)
-    assert target == EXPECTED_STATE
+    assert target == EXPECTED_STATE | already_present_dict
 
 
 def test_get_saved_sessions(tmp_path):
@@ -57,7 +65,11 @@ def test_save(mock_session_state, tmp_path):
     assert Path(file_path).exists()
 
 
-def test_save_and_load(mock_session_state, tmp_path):
+@patch("alphastats.gui.utils.session_manager.empty_session_state")
+@patch("alphastats.gui.utils.session_manager.init_session_state")
+def test_save_and_load(
+    mock_empty_session_state, mock_init_session_state, mock_session_state, tmp_path
+):
     """Test that the session state is saved and loaded correctly."""
 
     session_manager = SessionManager(str(tmp_path))
@@ -70,3 +82,5 @@ def test_save_and_load(mock_session_state, tmp_path):
     session_manager.load(Path(file_path).name, target)
 
     assert target == EXPECTED_STATE
+    mock_empty_session_state.assert_called_once()
+    mock_init_session_state.assert_called_once()
