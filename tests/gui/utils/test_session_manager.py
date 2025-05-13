f94af766827c23640e35a4d5ad0e6c98c9635d6c
mschwoerer <82171591+mschwoer@users.noreply.github.com>
2025-05-13 17:40:02 +0200

    add tests

--- tests/gui/utils/test_session_manager.py
+++ tests/gui/utils/test_session_manager.py
@@ -1,13 +1,14 @@
 """Tests for the SessionManager class."""
 
 from pathlib import Path
+from unittest import mock
 from unittest.mock import MagicMock, patch
 
 import pytest
 from streamlit.runtime.state import SessionStateProxy
 
 from alphastats.gui.utils.session_manager import SessionManager
-from alphastats.gui.utils.state_keys import StateKeys
+from alphastats.gui.utils.state_keys import LLMKeys, StateKeys
 
 EXPECTED_STATE = {
     StateKeys.USER_SESSION_ID: "some_id",
@@ -24,17 +25,32 @@ class MockSessionState(SessionStateProxy):
 
 
 @pytest.fixture
-def mock_llm_integration():
-    return MagicMock()
-
-
-@pytest.fixture
-def mock_session_state(mock_llm_integration):
+def mock_session_state():
     """Return a mock session state."""
 
     return MockSessionState(
         ignored_key="ignored_value",
         **EXPECTED_STATE,
+    )
+
+
+@pytest.fixture
+def mock_llm_integration():
+    return MagicMock()
+
+
+@pytest.fixture
+def mock_session_state_with_llm(mock_llm_integration):
+    """Return a mock session state."""
+
+    return MockSessionState(
+        ignored_key="ignored_value",
+        **EXPECTED_STATE,
+        **{
+            StateKeys.LLM_CHATS: {
+                "some_datetime": {LLMKeys.LLM_INTEGRATION: mock_llm_integration}
+            }
+        },
     )
 
 
@@ -94,11 +110,52 @@ def test_save_and_load(
         "some_key_that_will_get_overwritten": "some_value",
     }
 
-    current_session_state = llm_state.copy()
+    session_state_before_load = llm_state.copy()
 
     # when
-    session_manager.load(Path(file_path).name, current_session_state)
+    session_manager.load(Path(file_path).name, session_state_before_load)
 
-    assert current_session_state == EXPECTED_STATE | llm_state
+    assert session_state_before_load == EXPECTED_STATE | llm_state
     mock_empty_session_state.assert_called_once()
     mock_init_session_state.assert_called_once()
+
+
+@patch("alphastats.gui.utils.session_manager.empty_session_state")
+@patch("alphastats.gui.utils.session_manager.init_session_state")
+@patch("alphastats.gui.utils.session_manager.LLMClientWrapper")
+def test_save_and_load_with_llm(
+    mock_client_wrapper,
+    mock_init_session_state,
+    mock_empty_session_state,
+    mock_session_state_with_llm,
+    tmp_path,
+):
+    """Test that the session state is saved and loaded correctly when an LLM chat is present."""
+
+    session_manager = SessionManager(str(tmp_path))
+
+    file_path = session_manager.save(mock_session_state_with_llm)
+
+    llm_state = {
+        StateKeys.MODEL_NAME: "some_model_name",
+        StateKeys.OPENAI_API_KEY: "some_key",  # pragma: allowlist secret
+        StateKeys.BASE_URL: "some_url",
+        "some_key_that_will_get_overwritten": "some_value",
+    }
+
+    session_state_before_load = llm_state.copy()
+
+    # when
+    session_manager.load(Path(file_path).name, session_state_before_load)
+
+    assert session_state_before_load == EXPECTED_STATE | llm_state | {
+        StateKeys.LLM_CHATS: {"some_datetime": {LLMKeys.LLM_INTEGRATION: mock.ANY}}
+    }
+
+    mock_empty_session_state.assert_called_once()
+    mock_init_session_state.assert_called_once()
+    mock_client_wrapper.assert_called_once_with(
+        model_name="some_model_name",
+        api_key="some_key",  # pragma: allowlist secret
+        base_url="some_url",
+    )
