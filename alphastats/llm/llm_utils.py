a75110ffde0f4a0b3c2b908afcc8a1be2b7bfbc0
Julia Schessner <schessner@biochem.mpg.de>
2025-05-20 19:06:19 +0200

    Remove sample names from system message and only include str columns (e.g. no numerical clinical values)

--- alphastats/llm/llm_utils.py
+++ alphastats/llm/llm_utils.py
@@ -4,6 +4,8 @@ from typing import Dict
 
 import pandas as pd
 
+from alphastats.dataset.keys import Cols
+
 
 def get_subgroups_for_each_group(
     metadata: pd.DataFrame,
@@ -17,9 +19,16 @@ def get_subgroups_for_each_group(
     Returns:
         dict: A dictionary with the column names as keys and a list of unique values as values.
     """
-    groups = [str(group) for group in metadata.columns.to_list()]
+    groups = [
+        str(group)
+        for group in metadata.columns.to_list()
+        if metadata[group].dtype == "object"
+    ]
     group_to_subgroup_values = {
         group: [str(subgroup) for subgroup in metadata[group].unique().tolist()]
         for group in groups
     }
+    group_to_subgroup_values[Cols.SAMPLE] = (
+        f"{len(group_to_subgroup_values[Cols.SAMPLE])} samples"
+    )
     return group_to_subgroup_values
