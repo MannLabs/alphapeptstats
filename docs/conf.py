9c83aa8e8557992422c7bacf70a32c00a64c4e51
null <82171591+mschwoer@users.noreply.github.com>
2025-07-24 08:21:35 +0000

    Bump version: 0.7.1 → 0.7.2-dev0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Elena Krismer"
 author = "Elena Krismer"
 
 # The full version, including alpha/beta/rc tags
-release = "0.7.1"
+release = "0.7.2-dev0"
 
 
 # -- General configuration ---------------------------------------------------
